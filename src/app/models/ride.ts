87098a23d2080b7d9f9b89370c8b95fea6c2f3a8
Cade Ritter <ritter.cade@gmail.com>
2017-11-04 17:17:53 -0500

    add new ride and ride-detail component

--- src/app/models/ride.ts
+++ src/app/models/ride.ts
@@ -1,8 +1,14 @@
 import {User} from "./user";
 
 export class Ride {
+  _id: String;
   title: String;
   description: String;
+
+  departing_from: String;
+  arriving_at: String;
+  meeting_location: String;
+
   cost: Number;
   spots: Number;
   vehicle_type: String;
