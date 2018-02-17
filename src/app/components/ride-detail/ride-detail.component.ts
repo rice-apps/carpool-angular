7a8320b29875da0eabe560dc6b97ef6a009eedfa
dikshagupta414 <31869978+dikshagupta414@users.noreply.github.com>
2018-02-17 15:35:46 -0600

    ride detail work

--- src/app/components/ride-detail/ride-detail.component.ts
+++ src/app/components/ride-detail/ride-detail.component.ts
@@ -18,12 +18,53 @@ export class RideDetailComponent implements OnInit {
     this.route.params.subscribe(params => {
       this.rideService.getRide(params['_id'])
         .then(ride => this.ride = ride);
-    })
+    });
   }
 
   postUserToRide() {
     this.rideService.postUserToRide(this.ride._id)
       .then(ride => this.ride = ride);
+  }
+
+  departParseDay(rideTime: String) {
+    if (rideTime) {
+      return rideTime.split(' ')[1].slice(0, -1);
+    }
+    // console.log(rideTime.split(','));
+
+
+  }
+
+
+  departParseMonth(rideTime: String) {
+    if (rideTime) {
+      const temp =  rideTime.split(' ')[0];
+      if (temp.toLowerCase() === 'january') {
+        return 'Jan.';
+      } else if (temp.toLowerCase() === 'february') {
+        return 'Feb.';
+      } else if (temp.toLowerCase() === 'march') {
+        return 'Mar.';
+      } else if (temp.toLowerCase() === 'april') {
+        return 'Apr.';
+      } else if (temp.toLowerCase() === 'may') {
+        return 'May';
+      } else if (temp.toLowerCase() === 'june') {
+        return 'June';
+      } else if (temp.toLowerCase() === 'july') {
+        return 'July';
+      }  else if (temp.toLowerCase() === 'august') {
+        return 'Aug';
+      } else if (temp.toLowerCase() === 'september') {
+        return 'Sept.';
+      } else if (temp.toLowerCase() === 'october') {
+        return 'Oct.';
+      } else if (temp.toLowerCase() === 'november') {
+        return 'Nov.';
+      } else if (temp.toLowerCase() === 'december') {
+        return 'Dec.';
+      }
+    }
   }
 
 }
