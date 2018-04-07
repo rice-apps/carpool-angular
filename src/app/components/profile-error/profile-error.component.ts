45ae73d26a7eef8f8afcd8ad5b717f1da3166d2b
dikshagupta414 <31869978+dikshagupta414@users.noreply.github.com>
2018-04-07 16:36:32 -0500

    User profiles created

---
+++ src/app/components/profile-error/profile-error.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-error',
+  templateUrl: './profile-error.component.html',
+  styleUrls: ['./profile-error.component.scss']
+})
+export class ProfileErrorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
