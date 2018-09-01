660d79a92f7206e7cd04fcae2b577e0c51fdab71
myra <myraramdenbourg@gmail.com>
2018-09-01 17:04:05 -0500

    distinct landing pages

--- src/app/components/landing-page/landing-page.component.ts
+++ src/app/components/landing-page/landing-page.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import {CONFIG} from "../../config";
+import {AuthService} from '../../services/auth-service/auth.service';
+import {Observable} from 'rxjs/Observable';
+import {Router} from '@angular/router';
+
+
 
 @Component({
   selector: 'app-landing-page',
@@ -10,9 +15,17 @@ export class LandingPageComponent implements OnInit {
 
   private authUrl: string = `${CONFIG.cas_auth_url}?service=${CONFIG.service_url}`;
 
-  constructor() { }
+  loggedIn : Observable<boolean>;
+  constructor(private authService: AuthService, private router: Router) {
+  }
 
   ngOnInit() {
+    this.loggedIn = this.authService.isLoggedIn;
+  }
+
+  navigateToRides(){
+    this.router.navigate(['/rides'])
+      .then((success) => console.log(success));
   }
 
 
