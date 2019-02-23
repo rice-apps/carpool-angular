fcd11c4fa436e1261d413d1f46d1589ef9ceb9cb
myra <myraramdenbourg@gmail.com>
2019-02-23 16:10:03 -0600

    added comments schema

--- src/app/components/new-ride/new-ride.component.ts
+++ src/app/components/new-ride/new-ride.component.ts
@@ -29,15 +29,19 @@ export class NewRideComponent implements OnInit {
       departing_from: ['', Validators.required],
       date_time: ['', Validators.required],
       arriving_at: ['', Validators.required],
-      number_riders: ['', Validators.required]
+      number_riders: ['', Validators.required],
+      comments_input: ['']
     });
   }
   // when submit is pressed, the user information is updated and a new ride is created
+
   submit() {
     this.newRide.departing_from = this.newRideForm.value['departing_from'];
     this.newRide.arriving_at = this.newRideForm.value['arriving_at'];
     this.newRide.departing_datetime = this.newRideForm.value['date_time'];
     this.newRide.number_riders = this.newRideForm.value['number_riders'];
+    this.newRide.comments_input = this.newRideForm.value['comments_input'];
+
     this.rideService.addRide(this.newRide, this.authService.getCurrentUser()._id)
       .then((ride) => {
         this.router.navigate(['/rides', ride._id]);
