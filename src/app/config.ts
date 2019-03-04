5244b5617259d24556835d35ac65723a73d8fc44
dikshagupta414 <31869978+dikshagupta414@users.noreply.github.com>
2019-03-03 21:46:57 -0600

    changed config.js

--- src/app/config.ts
+++ src/app/config.ts
@@ -3,6 +3,5 @@ export const CONFIG = {
   api_url: 'https://carpool--31646.appspot.com/api',
   cas_auth_url: 'https://idp.rice.edu/idp/profile/cas/login',
   //service_url: 'http://localhost:4200/auth'
-  //service_url: 'http://carpool-riceapps.surge.sh/auth'
-  service_url: 'https://carpool-frontend.appspot.com/auth'
+  service_url: 'https://carpool.riceapps.org/auth'
 };
