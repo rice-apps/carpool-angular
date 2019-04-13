2ff31c8833f5fb31c5bf8174fcff54aad55d53ab
Angela Hwang <angie@angelas-mbp-2.dyndns.rice.edu>
2019-04-13 13:16:10 -0500

    /api

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,7 +1,7 @@
 export const CONFIG = {
  // api_url: 'http://localhost:3000/api',
-  api_url: 'https://carpool-backend-testing.appspot.com',
+  api_url: 'https://carpool-backend-testing.appspot.com/api',
   cas_auth_url: 'https://idp.rice.edu/idp/profile/cas/login',
-  //service_url: 'http://localhost:4200/auth'
+  // service_url: 'http://localhost:4200/auth'
   service_url: 'https://carpool-frontend-testing.appspot.com/auth'
 };
