ff0349884acf0673b4101ae9a59e4cb049571479
hwangange <hwangangela99@hotmail.com>
2020-01-12 15:00:43 +0600

    assets and about component

---
+++ src/app/components/about/about.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.scss']
+})
+export class AboutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+
+    const items = Array.from(document.querySelectorAll(".accordion a"));
+
+    function toggleAccordion(){
+      this.classList.toggle('active');
+      this.nextElementSibling.classList.toggle('active');
+    }
+
+    items.forEach(item => item.addEventListener('click', toggleAccordion));
+  }
+
+}
+
